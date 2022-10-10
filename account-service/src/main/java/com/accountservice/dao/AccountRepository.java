95bca420354c99798a62264d5143856270445c97
HanisRasid <s3781755@student.rmit.edu.au>
2022-10-11 01:19:33 +1100

    implement accountrepo

--- account-service/src/main/java/com/accountservice/dao/AccountRepository.java
+++ account-service/src/main/java/com/accountservice/dao/AccountRepository.java
@@ -1,5 +1,10 @@
 package com.accountservice.dao;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accountservice.model.Account;
+@Repository
+public interface AccountRepository  extends JpaRepository<Account,Integer>{
     
 }
