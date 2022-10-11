d86e312d64f31028d51cffa240b4c2a1c0a7f42e
HanisRasid <s3781755@student.rmit.edu.au>
2022-10-11 16:56:38 +1100

    implements exception haldling

--- person-service/src/main/java/com/personservice/controller/PersonController.java
+++ person-service/src/main/java/com/personservice/controller/PersonController.java
@@ -1,14 +1,15 @@
 package com.personservice.controller;
 
 import com.personservice.dao.PersonRepository;
+import com.personservice.exception.PersonNotFoundException;
 import com.personservice.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
@@ -32,69 +33,47 @@ public class PersonController {
     }
     @GetMapping("/persons/person{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable int id){
-        try{
-            Person personObj = getPersonInfo(id);
 
-            if (personObj != null){
-                
-                return new ResponseEntity<>(personObj, HttpStatus.OK);
-            }
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        catch (Exception e){
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Person personObj = repo.findById(id)
+            .orElseThrow(() -> new PersonNotFoundException("Not found person with id = " + id));
+
+        return new ResponseEntity<>(personObj, HttpStatus.OK);
     }
 
     @GetMapping("/persons/person")
     public ResponseEntity<List<Person>> getPersonList() {
-        try {
-            return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        List<Person> persons = new ArrayList<Person>();
+
+        repo.findAll().forEach(persons::add);
+
+        if (persons.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
+        
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<HttpStatus> deletePersonById(@PathVariable int id) {
-        try {
-
-            Person psn = getPersonInfo(id);
-
-            if (psn != null) {
-                repo.deleteById(id);
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        repo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/persons/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person person) {
 
-        Person personObj = getPersonInfo(id);
+        Person personObj = repo.findById(id)
+            .orElseThrow(() -> new PersonNotFoundException("Not found person with id = " + id));
 
-        if (personObj != null) {
-            personObj.setName(person.getName());
-            personObj.setAddress(person.getAddress());
-            personObj.setJob(person.getJob());
-            personObj.setPostcode(person.getPostcode());
-            personObj.setEmail(person.getEmail());
-            personObj.setPhoneno(person.getPhoneno());
-            personObj.setAge((person.getAge()));
-            return new ResponseEntity<>(repo.save(personObj), HttpStatus.OK);
-        }
+        personObj.setName(person.getName());
+        personObj.setAddress(person.getAddress());
+        personObj.setJob(person.getJob());
+        personObj.setPostcode(person.getPostcode());
+        personObj.setEmail(person.getEmail());
+        personObj.setPhoneno(person.getPhoneno());
+        personObj.setAge((person.getAge()));
 
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    private Person getPersonInfo(int id){
-        Optional<Person> personObj = repo.findById(id);
-
-        return personObj.orElse(null);
+        return new ResponseEntity<>(repo.save(personObj), HttpStatus.OK);
     }
 }
---
+++ person-service/src/main/java/com/personservice/exception/PersonNotFoundException.java
@@ -0,0 +1,8 @@
+package com.personservice.exception;
+
+public class PersonNotFoundException extends RuntimeException{
+    
+    public PersonNotFoundException(String msg){
+        super(msg);
+    }
+}
